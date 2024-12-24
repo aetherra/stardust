e2aab081facdfda6342b08b157fa200e2072bec8
zramctl <zerowatts@tuta.io>
2024-12-24 17:18:25 -0500

    fix: :rocket: forgot about rpi users

--- flake.nix
+++ flake.nix
@@ -39,7 +39,8 @@
       systems = [
         "x86_64-linux"
         "x86_64-darwin"
-        # who the hell has aarch64 in the big 24 bro like come on do better
+        "aarch64-linux"
+        "aarch64-darwin"
       ];
 
       perSystem =
