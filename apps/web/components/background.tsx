be58b1b203c5a1edde4df8c720282f660d1aa14e
tgt <tgt@incognitotgt.me>
2024-12-24 11:01:48 -0500

    ofn we are NOT using that

--- apps/web/components/background.tsx
+++
@@ -1,8 +0,0 @@
-export default function Background() {
-  return (
-    <>
-      <img alt="background" src={'/background.jpg'} className="h-screen w-screen fixed top-0 -z-40" />
-      <div className="h-screen w-screen  fixed top-0 -z-20" />
-    </>
-  )
-}
