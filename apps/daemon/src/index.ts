c4607b44eecf6840b54bb5e823b3081590f4d234
KAS <i@truekas.dev>
2025-01-21 18:12:55 -0500

    fix: forgot to cd to root when i committed :skull:
    Signed-off-by: KAS <i@truekas.dev>

--- apps/daemon/src/index.ts
+++ apps/daemon/src/index.ts
@@ -3,31 +3,32 @@ import { authCheck } from "~/auth-middleware";
 import { getConfig } from "~/lib/config";
 import { docker } from "~/lib/docker";
 import sessionHandler from "~/session";
+import { loadavg } from "node:os";
 const config = getConfig();
 export const app = new Elysia()
-	.get("/", async (c) => {
-		return {
-			message:
-				"✨ Stardust daemon by spaceness. \nSource tree: https://github.com/spaceness/stardust/tree/rewrite/apps/daemon",
-			success: true,
-			authenticated: (await authCheck(c))?.success !== false,
-		};
-	})
-	.onBeforeHandle(authCheck)
-	.onError(({ error, path }) => {
-		console.error(`✨ Stardust: [${path}] ${error}`);
-		return {
-			success: false,
-			error: error.toString(),
-		};
-	})
-	.get("/healthcheck", {
-		success: true,
-		cpu: process.cpuUsage(),
-		mem: process.memoryUsage(),
-		sessions: (await docker.listContainers()).filter((s) => s.HostConfig.NetworkMode === config.docker.network).length,
-	})
-	.use(sessionHandler);
+  .get("/", async (c) => {
+    return {
+      message:
+        "✨ Stardust daemon by spaceness. \nSource tree: https://github.com/spaceness/stardust/tree/rewrite/apps/daemon",
+      success: true,
+      authenticated: (await authCheck(c))?.success !== false,
+    };
+  })
+  .onBeforeHandle(authCheck)
+  .onError(({ error, path }) => {
+    console.error(`✨ Stardust: [${path}] ${error}`);
+    return {
+      success: false,
+      error: error.toString(),
+    };
+  })
+  .get("/healthcheck", {
+    success: true,
+    cpu: loadavg()[0],
+    mem: process.memoryUsage(),
+    sessions: (await docker.listContainers()).filter((s) => s.HostConfig.NetworkMode === config.docker.network).length,
+  })
+  .use(sessionHandler);
 
 // eden
 export type App = typeof app;
